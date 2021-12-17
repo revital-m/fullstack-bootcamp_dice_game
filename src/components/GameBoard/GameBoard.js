601090bb909f71a86ae387cce2be65eb63d39ca1
revital-m <revital13m@gmail.com>
2021-12-17 20:55:49 +0200

    added msg for double , double six & winner

--- src/components/GameBoard/GameBoard.js
+++ src/components/GameBoard/GameBoard.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import './GameBoard.css';
+import "./GameBoard.css";
 import GameData from "../GameData/GameData";
 import Player from "../Player/Player";
-import Title from "../Title/Title";
+// import Title from "../Title/Title";
+import Message from "../Message/Message";
 
 class GameBoard extends Component {
   state = {
@@ -12,6 +13,7 @@ class GameBoard extends Component {
     winner: "",
     iswin: false,
     isDouble: false,
+    isDoubleSix: false,
     currentScore0: 0,
     currentScore1: 0,
     totalScore0: 0,
@@ -59,37 +61,14 @@ class GameBoard extends Component {
     const dice2 = Math.floor(Math.random() * 6) + 1;
     const diceSum = dice1 + dice2;
 
-    this.setState({dices: [dice1, dice2]});
+    this.setState({ dices: [dice1, dice2] });
     setTimeout(() => {
-      this.resRollDice(dice1,dice2,diceSum);
+      this.resRollDice(dice1, dice2, diceSum);
     }, 500);
-
-    // if (dice1 === 6 && dice2 === 6) {
-    //   this.handleDoubleSix();
-    //   return;
-    // }
-    // this.setState((state) => {
-    //   if (state.playersTurn) {
-    //     return {
-    //       currentScore1: state.currentScore1 + diceSum,
-    //       dices: [dice1, dice2],
-    //     };
-    //   }
-    //   return {
-    //     currentScore0: state.currentScore0 + diceSum,
-    //     dices: [dice1, dice2],
-    //   };
-    // });
-    // if (dice1 !== dice2) {
-    //   this.handleHold();
-    // }
-    // else {
-    //   this.handleDouble();
-    // }
   };
 
   // update state.players-currentScore with the sum of the numbers to the current player.
-  resRollDice = (dice1,dice2,diceSum) => {
+  resRollDice = (dice1, dice2, diceSum) => {
     if (dice1 === 6 && dice2 === 6) {
       this.handleDoubleSix();
       return;
@@ -106,19 +85,18 @@ class GameBoard extends Component {
     });
     if (dice1 !== dice2) {
       this.handleHold();
-    }
-    else {
+    } else {
       this.handleDouble();
     }
-  }
+  };
 
   // do text massege on screen for rolling double.
   handleDouble = () => {
-    this.setState({isDouble: true});
+    this.setState({ isDouble: true });
     setTimeout(() => {
-      this.setState({isDouble: false});
-    }, 1500);
-  }
+      this.setState({ isDouble: false });
+    }, 1800);
+  };
 
   // reset the current player score and switch current player.
   handleDoubleSix = () => {
@@ -127,17 +105,20 @@ class GameBoard extends Component {
         return {
           currentScore1: 0,
           totalScore1: 0,
-          // dices: [6, 6],
           playersTurn: 0,
+          isDoubleSix:true,
         };
       }
       return {
         currentScore0: 0,
         totalScore0: 0,
-        // dices: [6, 6],
         playersTurn: 1,
+        isDoubleSix:true,
       };
     });
+    setTimeout(() => {
+      this.setState({ isDoubleSix: false });
+    }, 1800);
   };
 
   // update state.players-totalScore with the sum of the numbers to the current player. switch current player
@@ -182,7 +163,7 @@ class GameBoard extends Component {
           playerName={player}
           scoreTextTotal="Total:"
           scoreNumTotal={idx ? this.state.totalScore1 : this.state.totalScore0}
-          scoreTotalClass= "score-container--total"
+          scoreTotalClass="score-container--total"
           scoreTextCurrent="Current:"
           scoreNumCurrent={
             idx ? this.state.currentScore1 : this.state.currentScore0
@@ -199,19 +180,58 @@ class GameBoard extends Component {
     return (
       <div className="game-container">
         <main className="game-container__players">
-        {this.creatPlayers()}
-        <Title
-          TitleText={
-            this.state.isDouble ? `Congratulations you just rolled double, you get another turn!` : ``
-          }
-          TitleTextClass={this.state.isDouble ? `double` : ``}
-        />
-        <Title
-          TitleText={
-            this.state.iswin ? `${this.state.winner} is the winner!` : ``
-          }
-          TitleTextClass={this.state.iswin ? `winner` : ``}
-        />
+          {this.creatPlayers()}
+          <Message
+            msgClass={this.state.isDouble ? `double` : ``}
+            msgTitle={
+              this.state.isDouble
+                ? `Congratulations you just rolled double!`
+                : ``
+            }
+            msgTitleIcon={this.state.isDouble ? `fas fa-dice` : ``}
+            msgInfo={this.state.isDouble ? `you get another turn!` : ``}
+            msgInfoIcon={this.state.isDouble ? `far fa-grin-stars` : ``}
+          />
+          <Message
+            msgClass={this.state.isDoubleSix ? `double` : ``}
+            msgTitle={
+              this.state.isDoubleSix
+                ? `Oh no! you just rolled double 6...`
+                : ``
+            }
+            msgTitleIcon={this.state.isDoubleSix ? `fas fa-skull-crossbones` : ``}
+            msgInfo=""
+            msgInfoIcon=""
+          />
+          <Message
+            msgClass={this.state.iswin ? `winner` : ``}
+            msgTitle={
+              this.state.iswin ? `${this.state.winner} is the winner!` : ``
+            }
+            msgTitleIcon={
+              this.state.iswin ? `fas fa-trophy` : ``
+            }
+            msgInfo=""
+            msgInfoIcon=""
+          />
+          {/* <Title
+            TitleText={
+              this.state.isDouble
+                ? `Congratulations you just rolled double, you get another turn!`
+                : `Congratulations you just rolled double, you get another turn!`
+            }
+            TitleTextClass={this.state.isDouble ? `double` : `double`}
+            // TitleText={
+            //   this.state.isDouble ? `Congratulations you just rolled double, you get another turn!` : ``
+            // }
+            // TitleTextClass={this.state.isDouble ? `double` : ``}
+          />
+          <Title
+            TitleText={
+              this.state.iswin ? `${this.state.winner} is the winner!` : ``
+            }
+            TitleTextClass={this.state.iswin ? `winner` : ``}
+          /> */}
         </main>
         <GameData
           value={this.state.pointsToWin}
